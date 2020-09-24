d9b7667531c34ddfe01bcba26ac0b3c63aac77c8
JessVoss <jess_wolf_24@hotmail.com>
2020-09-24 12:56:44 -0400

    Add project files.

---
+++ MandalorianDB/Model/EpisodeOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MandalorianDB.BusinessLayer;
+
+namespace MandalorianDB.Model
+{
+    class EpisodeOperation
+    {
+        public enum OperationStatus { OKAY, CANCEL }
+        public OperationStatus Status { get; set; }
+        public Episode episode { get; set; }
+    }
+}
---
+++ MandalorianDB/Model/FileMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandalorianDB.Model
+{
+    public enum FileMessage
+    {
+        None,
+        Complete,
+        FileAccessError,
+        RecordNotFound,
+        NoRecordsFound
+    }
+}
